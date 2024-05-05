d75b79ac22a4039ae8b71fc33d188aa80e7cbdf1
Anh bui <buihoangducanh0987@gmail.com>
2024-05-06 03:04:09 +0700

    feature: add protected for route

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,9 +1,21 @@
+"use client";
 import SubTotal from "@/components/cart/sub-total";
 import CheckoutInfo from "@/components/checkout/checkout-info";
 import PaymentMethodSelection from "@/components/checkout/payment-method";
+import { useAppContext } from "@/contexts/app-context";
+import { useAuthContext } from "@/contexts/auth-context";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 export default function Checkout() {
+  const router = useRouter();
+  const { openLoginForm } = useAppContext();
+  const { user, isAuthenticating } = useAuthContext();
+  if (isAuthenticating) return null;
+  if (!user.isAuthenticated) {
+    router.push("/");
+    openLoginForm();
+  }
   return (
     <main>
       <div className="lg:mt-20 lg:grid lg:grid-cols-6 lg:gap-8">
